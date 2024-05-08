5ddf03eae7988be5b206c67e3c9b1f5d5b728ff8
Sabreena Daniyal <160657530+sabreenadaniyal@users.noreply.github.com>
2024-05-09 03:12:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+"use strict";
+//<><><><><><><><><><><><><><><><>ARROW FUNCTION<><><><><><><><><><><><><><><><><><<><><><><><><
+let myName = () => {
+    console.log("Sana");
+};
+myName();
+//---------------------------------Arrow function with Return-------------------------------------
+let Name1 = (name) => {
+    return (`Hello!${name},How are you?`);
+};
+let ans = Name1("Sabrena");
+console.log(ans);
+//----------------------------------Conditions-----------------------------------------------------
+//condition with return
+let isfeesPiad = (pay) => {
+    if (pay == "yes") {
+        return (true);
+    }
+    else {
+        return (false);
+    }
+};
+let answer = isfeesPiad("true");
+console.log(answer);
+//CALCULATOR
+let calc = (digit, digit2, sign) => {
+    if (sign == "+") {
+        console.log(digit + digit2);
+    }
+    else if (sign == "-") {
+        console.log(digit - digit2);
+    }
+    else if (sign == "*") {
+        console.log(digit * digit2);
+    }
+    else {
+        console.log(digit / digit2);
+    }
+};
+calc(5, 10, "+");
+calc(15, 5, "-");
+calc(2, 20, "*");
+calc(10, 2, "/");
+//---------------------------------------Block Scope(local scope)--------------------------------------------
+function uniStudent() {
+    let std = "Saqib";
+    console.log("Haseeb");
+}
+uniStudent();
+//---------------------------------Global Scope----------------------------------------------
+let globalPassport = "German_passport";
+let Pakistan = () => {
+    let pakistani_pass = "Pakistani-Passport";
+    console.log("Pakistani");
+    let globalPassport = "German_passport";
+    console.log(globalPassport);
+};
+let India = () => {
+    let Indian_pass = "Indian-Passport";
+    console.log("Indian");
+    let globalPassport = "German_passport";
+    console.log(globalPassport);
+};
+console.log(globalPassport);
+Pakistan();
+India();
